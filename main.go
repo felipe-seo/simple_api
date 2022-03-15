59664a86fd3ec7111f1e32aa3f7d9345b5a664f4
Felipe <felipe.seo@gmail.com>
2022-03-15 10:38:17 -0300

    changes to accounts, added part of transfers

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -21,8 +22,18 @@ type Account struct {
 	Created_at time.Time `json:"created_at"`
 }
 
+type Transfer struct {
+	Id                     string    `json:"id"`
+	Account_origin_id      string    `json:"account_origin_id"`
+	Account_destination_id string    `json:"account_destination_id"`
+	Amount                 int       `json:"amount"`
+	Created_at             time.Time `json:"created_at"`
+}
+
 //init accounts variable as a slice of accounts struct
 var accounts []Account
+
+var transfers []Transfer
 
 //get all accounts
 //w variable, r variable --> respons and request
@@ -34,7 +45,8 @@ func getAccounts(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(accounts)
 }
 
-//get a single account
+//get a single account(not used in the challenge, change it to specific balance only, also adjust the route)
+//handle error on unexistent account
 func getAccount(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	//we need the id, so we set the params variable
@@ -56,7 +68,17 @@ func createAccount(w http.ResponseWriter, r *http.Request) {
 	var account Account
 	_ = json.NewDecoder(r.Body).Decode(&account)
 	account.Id = uuid.New().String() //insert uuid
+
+	hashedSecret, err := HashPassword(account.Secret)
+	//tackle error handling
+	if err != nil {
+		//fmt.Errorf("error: %s", err)
+		fmt.Println(err)
+	}
+
+	account.Secret = hashedSecret
 	accounts = append(accounts, account)
+
 	json.NewEncoder(w).Encode(account)
 }
 
@@ -77,6 +99,7 @@ func updateAccount(w http.ResponseWriter, r *http.Request) {
 }
 
 //delete an account
+/*
 func deleteAccount(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -88,20 +111,44 @@ func deleteAccount(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(accounts)
 }
-
+*/
 //create a hash for the secret
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
 
+//used on login to check if the informed password translates to the stored hash
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
 
-//create uuid for accounts
+//transfers
+func getTransfers(w http.ResponseWriter, r *http.Request) {
+	//set header value of content type to application/json or else it's just gonna be served as text
+	w.Header().Set("Content-Type", "application/json")
 
+	//encode accounts variable as json in the response
+	json.NewEncoder(w).Encode(transfers)
+}
+
+func createTransfer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var transfer Transfer
+	_ = json.NewDecoder(r.Body).Decode(&transfer)
+	transfer.Id = uuid.New().String() //insert uuid - does it need to be unique?
+	fmt.Println(transfer.Account_origin_id)
+
+	transfers = append(transfers, transfer)
+
+	json.NewEncoder(w).Encode(transfer)
+}
+
+//validate transfer
+//login after dealing with transfers
+
+//
 func main() {
 	//initialize the router
 	r := mux.NewRouter()
@@ -112,14 +159,22 @@ func main() {
 	}
 
 	//data
-	accounts = append(accounts, Account{Id: "1", Name: "Boris Fausto", Cpf: 12355567812, Secret: hash, Balance: 100000, Created_at: time.Now()})
+	accounts = append(accounts, Account{Id: "dbd70daa-356c-4bde-be27-a13c3c47d44d", Name: "Boris Fausto", Cpf: 12355567812, Secret: hash, Balance: 100000, Created_at: time.Now()})
+	accounts = append(accounts, Account{Id: "dbd74daa-356c-4bde-be27-a13c3c47d49d", Name: "Doriana Yates", Cpf: 11111111506, Secret: hash, Balance: 5000, Created_at: time.Now()})
+
+	transfers = append(transfers, Transfer{Id: "1", Account_origin_id: "dbd70daa-356c-4bde-be27-a13c3c47d44d", Account_destination_id: "dbd74daa-356c-4bde-be27-a13c3c47d49d", Amount: 5000, Created_at: time.Now()})
 
 	//call router handlers(the functions) and establish endpoints(the route)
 	r.HandleFunc("/accounts", getAccounts).Methods("GET")
 	r.HandleFunc("/accounts/{id}", getAccount).Methods("GET")
 	r.HandleFunc("/accounts", createAccount).Methods("POST")
 	r.HandleFunc("/accounts/{id}", updateAccount).Methods("PUT")
-	r.HandleFunc("/accounts/{id}", deleteAccount).Methods("DELETE")
+	//r.HandleFunc("/accounts/{id}", deleteAccount).Methods("DELETE")
+
+	//transfer
+	r.HandleFunc("/transfers", getTransfers).Methods("GET")
+	r.HandleFunc("/transfers", createTransfer).Methods("POST")
+
 	//Wrapped with log, throws error in case it fails					receives a port and a router
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
