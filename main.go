78bb5386a5d855c775723c7b0daa6d31f67eb142
Felipe <felipe.seo@gmail.com>
2022-03-18 14:43:09 -0300

    transfers are complete

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ type Transfer struct {
 	Id                     string    `json:"id"`
 	Account_origin_id      string    `json:"account_origin_id"`
 	Account_destination_id string    `json:"account_destination_id"`
-	Amount                 int       `json:"amount"`
+	Amount                 int64     `json:"amount"`
 	Created_at             time.Time `json:"created_at"`
 }
 
@@ -50,6 +50,21 @@ func getAccounts(w http.ResponseWriter, r *http.Request) {
 func getAccount(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	//we need the id, so we set the params variable
+	params := mux.Vars(r) //get params
+
+	//loop through accounts and find the one with a matching id
+	for _, item := range accounts {
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Account{})
+}
+
+//get the balance of an account
+func getAccountBalance(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) //get params
 
 	//loop through accounts and find the one with a matching id
@@ -138,14 +153,48 @@ func createTransfer(w http.ResponseWriter, r *http.Request) {
 	var transfer Transfer
 	_ = json.NewDecoder(r.Body).Decode(&transfer)
 	transfer.Id = uuid.New().String() //insert uuid - does it need to be unique?
-	fmt.Println(transfer.Account_origin_id)
 
-	transfers = append(transfers, transfer)
+	if validateTransfer(transfer) {
+		//transfer funds
+
+		//register the transfer
+		transfers = append(transfers, transfer)
+
+	}
 
 	json.NewEncoder(w).Encode(transfer)
+	//this is just part of it, I still need to seek both accounts and validade the transaction
 }
 
 //validate transfer
+
+func validateTransfer(transfer Transfer) bool {
+	for _, v := range accounts {
+		//verify the existence of the destination id, no need to check the origin if logged in
+		if v.Id == transfer.Account_destination_id {
+			fmt.Println("Destination account exists " + transfer.Account_destination_id)
+			for _, v2 := range accounts {
+				//verify the balance on the origin account
+				if v2.Id == transfer.Account_origin_id && v2.Balance >= transfer.Amount {
+					fmt.Printf("\n %d", v2.Balance)
+					fmt.Printf("\nOrigin account balance: %d, transfer accepted", v2.Balance)
+					v2.Balance -= transfer.Amount
+					v.Balance += transfer.Amount
+					fmt.Printf("\nOrigin account balance after transfer: %d", v2.Balance)
+
+					return true
+				} else {
+					fmt.Printf("\nOrigin account balance is insufficient (%d)", v2.Balance)
+					return false
+				}
+			}
+		}
+	}
+	fmt.Printf("\nDestination account %s does not exist \n", transfer.Account_destination_id) //wrong destination account doesn't exist AT THIS entry
+
+	return false
+}
+
 //login after dealing with transfers
 
 //
@@ -159,14 +208,15 @@ func main() {
 	}
 
 	//data
-	accounts = append(accounts, Account{Id: "dbd70daa-356c-4bde-be27-a13c3c47d44d", Name: "Boris Fausto", Cpf: 12355567812, Secret: hash, Balance: 100000, Created_at: time.Now()})
-	accounts = append(accounts, Account{Id: "dbd74daa-356c-4bde-be27-a13c3c47d49d", Name: "Doriana Yates", Cpf: 11111111506, Secret: hash, Balance: 5000, Created_at: time.Now()})
+	accounts = append(accounts, Account{Id: "dbd74daa-356c-4bde-be27-a13c3c47d49d", Name: "Doriana Yates", Cpf: 11111111506, Secret: hash, Balance: 50, Created_at: time.Now()})
+	accounts = append(accounts, Account{Id: "dbd70daa-356c-4bde-be27-a13c3c47d44d", Name: "Boris Fausto", Cpf: 12355567812, Secret: hash, Balance: 100, Created_at: time.Now()})
 
 	transfers = append(transfers, Transfer{Id: "1", Account_origin_id: "dbd70daa-356c-4bde-be27-a13c3c47d44d", Account_destination_id: "dbd74daa-356c-4bde-be27-a13c3c47d49d", Amount: 5000, Created_at: time.Now()})
 
 	//call router handlers(the functions) and establish endpoints(the route)
 	r.HandleFunc("/accounts", getAccounts).Methods("GET")
 	r.HandleFunc("/accounts/{id}", getAccount).Methods("GET")
+	r.HandleFunc("/accounts/{id}/balance", getAccountBalance).Methods("GET")
 	r.HandleFunc("/accounts", createAccount).Methods("POST")
 	r.HandleFunc("/accounts/{id}", updateAccount).Methods("PUT")
 	//r.HandleFunc("/accounts/{id}", deleteAccount).Methods("DELETE")
